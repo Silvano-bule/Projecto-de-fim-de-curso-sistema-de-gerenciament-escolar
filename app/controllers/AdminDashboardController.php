3e1a5aebf63d0c4e029e46dce483a8cae547d782
Silvano-bule <silvanobule520@gmail.com>
2025-12-16 15:22:33 +0100

    Salvanddo as alterações

--- app/controllers/AdminDashboardController.php
+++ app/controllers/AdminDashboardController.php
@@ -9,6 +9,16 @@ class AdminDashboardController
     public function render()
     {
         AuthController::iniciarSessao();
+
+        if(AuthController::isLogged() !== true){
+            header("Location: index.php?page=entrar");
+            exit;
+        }
+
+        if(!isset($_SESSION['user_tipo']) === 'admin'){
+            header("Location: index.php?page=entrar");
+            exit;
+        }
         require_once __DIR__ . '/../views/adminDashboardView.php';
     }
 }
